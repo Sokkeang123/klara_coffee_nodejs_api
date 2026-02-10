fe0ab1926c86d0daab58996cd26be5e21584f136
Hi i'm sokkeang <sokkeangsieb27225@gmail.com>
2026-02-10 15:29:42 +0700

    update server file

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const cors = require('cors');
-const swaggerUi = require('swagger-ui-express');
-const swaggerJsdoc = require('swagger-jsdoc');
+const express = require("express");
+const cors = require("cors");
+const swaggerUi = require("swagger-ui-express");
+const swaggerJsdoc = require("swagger-jsdoc");
 
 const app = express();
 app.use(express.json());
@@ -10,35 +10,37 @@ app.use(cors());
 // Swagger options
 const options = {
   definition: {
-    openapi: '3.0.0',
+    openapi: "3.0.0",
     info: {
-      title: 'Klara Coffee API',
-      version: '1.0.0',
-      description: 'API documentation for Klara Coffee project',
+      title: "Klara Coffee API",
+      version: "1.0.0",
+      description: "API documentation for Klara Coffee project",
     },
     servers: [
       {
-        url: 'http://localhost:5000',
+        // url: 'http://localhost:5000',
+        url: "http://192.168.1.15:5000",
+        description: "Development server",
       },
     ],
   },
-  apis: ['./routes/*.js'], // path to your route files
+  apis: ["./routes/*.js"], // path to your route files
 };
 
 const specs = swaggerJsdoc(options);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 
 // Import routes
-const authRoutes = require('./routes/auth');
-const menuRoutes = require('./routes/menu');
-const orderRoutes = require('./routes/order');
+const authRoutes = require("./routes/auth");
+const menuRoutes = require("./routes/menu");
+const orderRoutes = require("./routes/order");
 
-app.use('/api/auth', authRoutes);
-app.use('/api/menu', menuRoutes);
-app.use('/api/orders', orderRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/menu", menuRoutes);
+app.use("/api/orders", orderRoutes);
 
-app.get('/', (req, res) => {
-  res.send('Klara Coffee API with MySQL is running...');
+app.get("/", (req, res) => {
+  res.send("Klara Coffee API with MySQL is running...");
 });
 
-app.listen(5000, () => console.log('Server running on port 5000'));
+app.listen(5000, () => console.log("Server running on port 5000"));
